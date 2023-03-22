f36bfb42c9840b18fb9624c28cf97c43fd21a7bb
axteria <fayee.wu@hotmail.com>
2023-03-22 22:08:23 +1100

    add preset Paris JS

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ let laTime = moment.tz("America/Los_Angeles");
 
 laDateElement.innerHTML = laTime.format("MMMM	Do YYYY");
 laTimeElement.innerHTML = laTime.format("h:mm:ss [<small>]A[</small>]");
+
+// PARIS
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment.tz("Europe/Paris");
+
+parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
