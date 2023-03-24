839fb7ea3f1b6b5d9f2c7a9bc11040e546964bf9
axteria <fayee.wu@hotmail.com>
2023-03-24 17:13:49 +1100

    added set interval / update seconds

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // LA
-let laElement = document.querySelector("#la");
-let laDateElement = laElement.querySelector(".date");
-let laTimeElement = laElement.querySelector(".time");
-let laTime = moment.tz("America/Los_Angeles");
+function updateTime() {
+  let laElement = document.querySelector("#la");
+  let laDateElement = laElement.querySelector(".date");
+  let laTimeElement = laElement.querySelector(".time");
+  let laTime = moment.tz("America/Los_Angeles");
 
-laDateElement.innerHTML = laTime.format("MMMM	Do YYYY");
-laTimeElement.innerHTML = laTime.format("h:mm:ss [<small>]A[</small>]");
+  laDateElement.innerHTML = laTime.format("MMMM	Do YYYY");
+  laTimeElement.innerHTML = laTime.format("h:mm:ss [<small>]A[</small>]");
 
-// PARIS
-let parisElement = document.querySelector("#paris");
-let parisDateElement = parisElement.querySelector(".date");
-let parisTimeElement = parisElement.querySelector(".time");
-let parisTime = moment.tz("Europe/Paris");
+  // PARIS
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment.tz("Europe/Paris");
 
-parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
-parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+  parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+// INTERVAL SECONDS
+setInterval(updateTime, 1000);
